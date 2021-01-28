cebd83957519f4a58f49f21a2480a9d93ad1da1e
skuttenkuler <sam.kuttenk@gmail.com>
2021-01-28 14:07:59 -0800

    initial, add hacker script, loaded assets, game plays, printing to game terminal window

---
+++ Assets/Hacker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+    #region ShowMainMenu
+    void ShowMainMenu()
+    {
+        Terminal
+        Terminal.WriteLine("Want to play a game?");
+        Terminal.WriteLine("Let's hack something...");
+        Terminal.WriteLine("Press 1 for your local Library");
+        Terminal.WriteLine("Press 2 for the local police station");
+        Terminal.WriteLine("Enter your selection:");   
+    }
+    #endregion
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
